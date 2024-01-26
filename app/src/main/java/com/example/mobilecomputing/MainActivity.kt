04b5900892623dc1ee47699cc0d864bbb73bab47
NhanDNguyen <nhannguyenduc96@gmail.com>
2024-01-26 14:58:40 +0200

    Add request permission for notification

--- app/src/main/java/com/example/mobilecomputing/MainActivity.kt
+++ app/src/main/java/com/example/mobilecomputing/MainActivity.kt
@@ -2,22 +2,20 @@ package com.example.mobilecomputing
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.room.Room
 import com.example.mobilecomputing.data.ProfileDatabase
 import com.example.mobilecomputing.notification.NotificationService
+import com.example.mobilecomputing.notification.REQUEST_CODE
 import com.example.mobilecomputing.ui.theme.MobileComputingTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,6 +33,18 @@ class MainActivity : ComponentActivity() {
                 "Changing Theme",
                 NotificationManager.IMPORTANCE_HIGH
             )
+            with(NotificationManagerCompat.from(applicationContext)) {
+                if (ActivityCompat.checkSelfPermission(
+                        this@MainActivity,
+                        android.Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED)  {
+                    ActivityCompat.requestPermissions(
+                        this@MainActivity,
+                        arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+                        REQUEST_CODE
+                    )
+                }
+            }
             notificationService.notificationManager.createNotificationChannel(notificationChannel)
             val profileDao = db.profileDao()
             val viewModel = AppViewModel(profileDao, notificationService)
